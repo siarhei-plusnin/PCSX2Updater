cadc3397495b32350df241ea33aee6721112fc24
Siarhei Plusnin <siarhei.plusnin@gmail.com>
2022-01-16 23:44:45 +0300

    Mirgated from buildbot to github releases

--- UpdatePCSX2.ps1
+++ UpdatePCSX2.ps1
@@ -1,63 +1,55 @@
-#Define PCSX2 path
+#Define PCSX2 path, repo url and version needed
 $runFolder = Split-Path $MyInvocation.MyCommand.Path -Parent
-$buildbotUrl = 'https://buildbot.orphis.net/pcsx2/'
+$githubUrl = 'https://api.github.com/repos/PCSX2/pcsx2/releases'
+$releaseType = '*' + '64bit-AVX2.7z'
 
 function DownloadAndExtractNewVersion {
-    param (
-        [string]$DownloadUrl,
-        [string]$Version
-    )
+    param ([string]$DownloadUrl, [string]$Version)
     
     #Define paths
-    $latestBuildPath = $runFolder + '\latest'
-    $archivePath = $latestBuildPath + '\pcsx2.7z'
-    $latestVersionFilePath = $latestBuildPath + '\pcsx2latest\version.ini'
-    $portablePath = $latestBuildPath + '\pcsx2latest\portable.ini'
+    $tempDirectory = $runFolder + '\latest'
+    $archivePath = $tempDirectory + '\pcsx2.7z'
+    $latestBuildDirectory = $tempDirectory + '\pcsx2latest'
+    $latestVersionFilePath = $latestBuildDirectory + '\version.ini'
     
     #Create temp folder for new version
-    New-Item -Path $latestBuildPath -ItemType 'directory'
+    New-Item -Path $tempDirectory -ItemType 'directory'
 
-    #Download PCSX2
+    #Download and extract PCSX2
     Invoke-WebRequest $DownloadUrl -OutFile $archivePath
+    Expand-7Zip -ArchiveFileName $archivePath -TargetPath $latestBuildDirectory
 
-    #Extract downloaded archive to latest folder
-    Expand-7Zip -ArchiveFileName $archivePath -TargetPath $latestBuildPath
-
-    #Rename extracted folder
-    Get-ChildItem $latestBuildPath -Exclude pcsx2.7z | Rename-Item -NewName 'pcsx2latest'
-    
     #Write version to file
     Set-Content -Path $latestVersionFilePath -Value $Version
 
-    #Overwrite portable.ini contents
-    Set-Content -Path $portablePath -Value 'RunWizard=0'
-
     #Copy/Overwrite what is in the emulator folder
-    Copy-Item -Path ($latestBuildPath + '\pcsx2latest\*') -Destination $runFolder -Force -Recurse -PassThru
+    Copy-Item -Path ($latestBuildDirectory + '\*') -Destination $runFolder -Force -Recurse -PassThru
 
     #Delete downloaded PCSX2 files when done to prep for next version
-    Remove-Item -Path $latestBuildPath -recurse
+    Remove-Item -Path $tempDirectory -recurse
 }
 
 #Entry point
-(Invoke-WebRequest $buildbotUrl).Content.Split("`n") | Where-Object { $_ -match '>download<' } | Select-Object -f 1 | ForEach-Object {
-    #Get latest version name
-    $newVersion = $_.split('"')[1]
+$releases = Invoke-WebRequest $githubUrl | ConvertFrom-Json
 
-    #Get current version present, if any
-    if (Test-Path ($runFolder + '\version.ini')) {
-        $currentVersion = Get-Content -Path ($runFolder + '\version.ini')
-    }
+#Get latest version name
+$newVersion = $releases[0].tag_name
 
-    #Compare version names; if our version is not the most recent one, then download it
-    if ($currentVersion -ne $newVersion) {
-        $downloadUrl = 'https://buildbot.orphis.net' + $_.split("'")[1].replace('amp;', '')
-        DownloadAndExtractNewVersion -DownloadUrl $downloadUrl -Version $newVersion
-        Write-Host 'PCSX2 updated'
-    }
-    else {
-        Write-Host 'PCSX2 already up to date'
-    }
+#Get download url
+$downloadUrl = ($releases[0].assets | Where-Object { $_.name -like $releaseType }).browser_download_url
+
+#Get current version present, if any
+if (Test-Path ($runFolder + '\version.ini')) {
+    $currentVersion = Get-Content -Path ($runFolder + '\version.ini')
+}
+
+#Compare version names; if our version is not the most recent one, then download it
+if ($currentVersion -ne $newVersion) {
+    DownloadAndExtractNewVersion -DownloadUrl $downloadUrl -Version $newVersion
+    Write-Host 'PCSX2 updated'
+}
+else {
+    Write-Host 'PCSX2 already up to date'
 }
 
 Read-Host
--- readme.md
+++ readme.md
@@ -1,11 +1,21 @@
 # PCSX2 updater
 
 Small PCSX2 updater written with PowerShell using [7Zip4Powershell](https://github.com/thoemmi/7Zip4Powershell)
-Based on [UpdatePCSX2.ps1](https://github.com/jpcarstech/emulation/blob/main/UpdatePCSX2.ps1) by [jpcarstech](https://github.com/jpcarstech).
+Based on [UpdatePCSX2.ps1](https://github.com/jpcarstech/emulation/blob/main/UpdatePCSX2.ps1) by [jpcarstech](https://github.com/jpcarstech)
+Updater downloads any latest release (including pre-releases) directly from GitHub using GitHub Api
 
 ## Usage
-- Copy script to some directory on your machine  (PCSX2 folder or empty one)
 - Install 7Zip4Powershell with `Install-Module -Name 7Zip4Powershell` in powershell console
+- Copy script to some directory on your machine  (PCSX2 folder or empty one)
+- change `$releaseType` to the one you need: 
+  - `32bit-AVX2-symbols.7z`
+  - `32bit-AVX2.7z`
+  - `32bit-SSE4-symbols.7z`
+  - `32bit-SSE4.7z`
+  - `64bit-AVX2-symbols.7z`
+  - `64bit-AVX2.7z`
+  - `64bit-SSE4-symbols.7z`
+  - `64bit-SSE4.7z`
 - right click on script -> *Run with PowerShell*
 - ???
 - PROFIT
